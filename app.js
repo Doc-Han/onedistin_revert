5ea2367f4552579cae9ca7329a4d5e804d21bbda
doc-han <yahyafarhan48@gmail.com>
2018-09-07 22:51:50 +0000

    another

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
-    res.send('error');
+    res.render('error');
 });
 
 //listening to a specific port
