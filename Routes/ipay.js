06c387ff03d8702487f1155554b6064ce1b66f11
doc-han <yahyafarhan48@gmail.com>
2018-09-08 18:53:15 +0000

    rebuilding Ipay's redirect pages

--- Routes/ipay.js
+++ Routes/ipay.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/p_s', (req,res) =>{
-  res.send("Your payment is now being approved!");
+  res.render("ipay/p_s");
 });
 
 router.get('/p_c', (req,res) =>{
-  res.send("Please do you have any reason for cancelling our payment. Please let us know. call: 02444444444");
+  res.render("ipay/p_c");
 });
 
 router.get('/d_p', (req,res) =>{
