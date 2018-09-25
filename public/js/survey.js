e595afcc522e26b21d77562fd15aa2b0efeb262a
doc-han <yahyafarhan48@gmail.com>
2018-09-25 23:08:27 +0000

    changes 122

--- public/js/survey.js
+++ public/js/survey.js
@@ -16,22 +16,22 @@ $(document).ready(function(){
           var s_total = (survey.ans_one_no*1) + (survey.ans_two_no*1) + (survey.ans_three_no*1) + (survey.ans_four_no*1) + (survey.ans_five_no*1) + (survey.ans_six_no*1);
           var s_html = '<h5 style="font-size:22px" class="muli-regular"> '+survey.question+' </h5>';
           if(survey.ans_one){
-          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-padding w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_one_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_one+' - '+(((survey.ans_one_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
+          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_one_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_one+' - '+(((survey.ans_one_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
           }
           if(survey.ans_two){
-          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-padding w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_two_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_two+' - '+(((survey.ans_two_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
+          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_two_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_two+' - '+(((survey.ans_two_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
           }
           if(survey.ans_three){
-          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-padding w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_three_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_three+' - '+(((survey.ans_three_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
+          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_three_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_three+' - '+(((survey.ans_three_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
           }
           if(survey.ans_four){
-          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-padding w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_four_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_four+' - '+(((survey.ans_four_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
+          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_four_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_four+' - '+(((survey.ans_four_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
           }
           if(survey.ans_five){
-          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-padding w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_five_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_five+' - '+(((survey.ans_five_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
+          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_five_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_five+' - '+(((survey.ans_five_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
           }
           if(survey.ans_six){
-          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-padding w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_six_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_six+' - '+(((survey.ans_six_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
+          s_html += '<div style="position:relative" class="w3-row"><div class="w3-col w3-blue" style="height:40px;overflow-x: hidden;margin-bottom:5px;width:'+((survey.ans_six_no*1)/s_total)*100+'%"><div style="position:absolute;top:0;right:0;left:3%;bottom:0;line-height:40px;">'+survey.ans_six+' - '+(((survey.ans_six_no*1)/s_total)*100).toFixed(0)+'%</div></div></div>';
           }
 
           $("#survey").html(s_html);
