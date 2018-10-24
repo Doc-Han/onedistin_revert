237072a0cee64daa64c63e3dc3911979edafea83
doc-han <yahyafarhan48@gmail.com>
2018-10-24 11:26:32 +0000

    sending message on signup visit

--- Routes/main.js
+++ Routes/main.js
@@ -9,6 +9,7 @@ var tokenGen = require('../config/tools.js');
 var cloudinary = require('cloudinary');
 var mailer = require('../config/nodemailer.js');
 var rp = require('request-promise');
+const sgMail = require('@sendgrid/mail');
 var router = express.Router();
 
 passport.use(new localStrategy(
@@ -125,6 +126,16 @@ router.post('/login', isNotLoggenIn, passport.authenticate('local', {
 }));
 
 router.get('/signup', isNotLoggenIn, (req,res) => {
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: 'yahyafarhan48@gmail.com',
+    from: 'Onedistin',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
   if(req.query.referer){
     var referer = req.query.referer;
   }else{
